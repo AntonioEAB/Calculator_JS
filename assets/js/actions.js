5e4416789895353e9fead4e92e1e66fbd87de677
AntonioEAB <antonioaviles908@gmail.com>
2024-03-08 10:09:15 -0600

    Changes in the script

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -3,3 +3,21 @@ Developer: Antonio Aviles
 Date: 04/03/2024
 Github: AntonioEAB
 */
+let inputs = document.querySelectorAll("input[type='button']");
+
+inputs.forEach(input => {
+    input.addEventListener("click", event => {
+        let display = document.getElementById('number');
+        let opt = event.target.value;
+        // Concatenar el valor del botón al valor actual del display
+        
+        if (opt === "=" && display.match(/\d$/) ){
+            display.value= eval(display.value);
+        } else {
+            display.value += opt;
+        }
+    });
+});
+
+
+
--- assets/js/operations.js
+++
@@ -1,5 +0,0 @@
-/*
-Developer: Antonio Aviles
-Date: 04/03/2024
-Github: AntonioEAB
-*/
