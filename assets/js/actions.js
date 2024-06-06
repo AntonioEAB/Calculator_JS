16f2776ce12f512a43632ca50d5c972bf6a2f473
AntonioEAB <antonioaviles908@gmail.com>
2024-06-06 15:44:38 -0600

    New updates

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,26 +1,54 @@
 /*
 Developer: Antonio Aviles
-Date: 04/03/2024
+Date: 06/06/2024
 Github: AntonioEAB
 */
+
 let inputs = document.querySelectorAll("input[type='button']");
+let display = document.getElementById('number');
 
 inputs.forEach(input => {
     input.addEventListener("click", event => {
-        let display = document.getElementById('number');
-        let opt = event.target.value;
-        // Concatenar el valor del botón al valor actual del display
-        
-        if (opt === "=" ){
-            if (String(display).match(/\d$/)) {
+        let inputValue = event.target.value;
 
-            }
-            display.value= eval(display.value);
+        if (inputValue === '=') {
+            evaluate();
+        } else if (inputValue === 'CE') {
+            clearDisplay();
         } else {
-            display.value += opt;
+            appendToDisplay(inputValue);
         }
     });
 });
 
 
 
+function evaluate() {
+    const expression = display.value.trim();
+
+    // Verifica si la entrada es una expresión aritmética válida
+    if (isArithmeticExpression(expression)) {
+        try {
+            const result = eval(expression);
+            display.value = result;
+        } catch (error) {
+            display.value = 'Error';
+        }
+    } else {
+        display.value = 'Expresión inválida';
+    }
+}
+
+function isArithmeticExpression(expression) {
+    // Expresión regular para validar una expresión aritmética
+    const arithmeticExpression = /^[\d\s\+\-\*\/\(\)\.]+$/;
+    return arithmeticExpression.test(expression);
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function appendToDisplay(value) {
+    display.value += value;
+}
